fdc3c5d123b3a8599fd1647e2522574938199cd7
noreal-3435 <santiago.nole@tikee.tech>
2024-09-01 17:28:04 -0500

    Feat: Limpieza de interfaces no utilizadas

--- src/main/java/org/client_person_service/client_person_service/application/interfaces/GetAllCustomersService.java
+++
@@ -1,8 +0,0 @@
-package org.client_person_service.client_person_service.application.interfaces;
-
-import org.client_person_service.client_person_service.application.dto.CustomerDTO;
-import reactor.core.publisher.Flux;
-
-public interface GetAllCustomersService {
-    Flux<CustomerDTO> getAllCustomers();
-}
